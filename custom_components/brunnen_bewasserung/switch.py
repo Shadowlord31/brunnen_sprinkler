8050acaf4b854d60b17848d8dbc00c9387659063
Shadowlord31 <99006900+Shadowlord31@users.noreply.github.com>
2026-07-05 16:57:53 +0200

    feat(switch): WindIgnoreSwitch als togglebarer Schalter

--- custom_components/brunnen_bewasserung/switch.py
+++ custom_components/brunnen_bewasserung/switch.py
@@ -7,7 +7,7 @@ from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN, CONF_INSTANCE_NAME, CONF_ENTRY_TYPE, ENTRY_TYPE_ZONE
+from .const import DOMAIN, CONF_INSTANCE_NAME, CONF_ENTRY_TYPE, ENTRY_TYPE_ZONE, CONF_IGNORE_WIND, DEFAULT_IGNORE_WIND
 from .coordinator import BrunnenBewasserungCoordinator
 
 
@@ -23,7 +23,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     if entry.data.get(CONF_ENTRY_TYPE) != ENTRY_TYPE_ZONE:
         return
     coordinator: BrunnenBewasserungCoordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([AutomatikSwitch(coordinator, entry)])
+    async_add_entities([AutomatikSwitch(coordinator, entry), WindIgnoreSwitch(coordinator, entry)])
 
 
 class AutomatikSwitch(CoordinatorEntity[BrunnenBewasserungCoordinator], SwitchEntity):
@@ -45,3 +45,32 @@ class AutomatikSwitch(CoordinatorEntity[BrunnenBewasserungCoordinator], SwitchEn
 
     async def async_turn_off(self, **kwargs) -> None:
         await self.coordinator.async_set_auto_enabled(False)
+
+
+class WindIgnoreSwitch(CoordinatorEntity[BrunnenBewasserungCoordinator], SwitchEntity):
+    _attr_icon = "mdi:weather-windy-variant"
+
+    def __init__(self, coordinator, entry):
+        super().__init__(coordinator)
+        self._entry = entry
+        self._attr_unique_id = f"{entry.entry_id}_ignore_wind"
+        self._attr_name = "Windpause ignorieren"
+        self._attr_device_info = _device_info(entry)
+
+    @property
+    def is_on(self) -> bool:
+        opts = dict(self._entry.data)
+        opts.update(self._entry.options)
+        return bool(opts.get(CONF_IGNORE_WIND, DEFAULT_IGNORE_WIND))
+
+    async def async_turn_on(self, **kwargs) -> None:
+        new_options = dict(self._entry.options)
+        new_options[CONF_IGNORE_WIND] = True
+        self.hass.config_entries.async_update_entry(self._entry, options=new_options)
+        self.coordinator.async_update_listeners()
+
+    async def async_turn_off(self, **kwargs) -> None:
+        new_options = dict(self._entry.options)
+        new_options[CONF_IGNORE_WIND] = False
+        self.hass.config_entries.async_update_entry(self._entry, options=new_options)
+        self.coordinator.async_update_listeners()
